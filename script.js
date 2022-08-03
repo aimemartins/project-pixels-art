0a9198c0c558b66e6aa077a0a0157ca3cdcde2f5
AIMÊ <aimemartins@hotmail.com>
2022-08-03 11:05:14 -0300

    Requisito 6 e 7 finalizados

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-window.onload = () => {
+// Requisito 6
+window.onload = function () {
   function onloadColor() {
     const firstColor = document.getElementById('black');
     firstColor.classList.add('selected');
   }
+
   onloadColor();
 
   // Criar selected
-  function changeSelected(event) {
-    const colorSelected = document.getElementsByClassName('selected');
-    colorSelected.classList.remove('selected');
-    event.target.classList.add('selected');
+  function captureColor(event) {
+    const clickColor = event.target;
+    const colorCapture = document.querySelector('.selected');
+    colorCapture.classList.remove('selected');
+    clickColor.classList.add('selected');
   }
-  // Capturar elemento que será pai
-  const colorPalette = document.getElementById('color-palette');
-  // Chama o evento
-  colorPalette.addEventListener('click', changeSelected);
+  const changeColor = document.getElementById('color-palette');
+  changeColor.addEventListener('click', captureColor);
 };
